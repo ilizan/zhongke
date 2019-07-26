ed5c16c9ad1dfd0a8ebcc9381aefc3b1205b7ade
ilizan <442926536@qq.com>
2019-07-26 18:27:20 +0800

    任务模块

--- ant/src/views/data/task.vue
+++ ant/src/views/data/task.vue
@@ -12,13 +12,36 @@
     </div>
 
     <div class="dataList">
-        
+      <a-row :gutter="20">
+        <a-col :span="8">
+            <a-button type="primary" @click="isShow = true">添加任务</a-button>
+        </a-col>
+      </a-row>
+      <a-tabs defaultActiveKey="1">
+        <a-tab-pane tab="任务1" key="1">任务1</a-tab-pane>
+        <a-tab-pane tab="任务2" key="2" forceRender>任务2</a-tab-pane>
+        <a-tab-pane tab="任务3" key="3">任务3</a-tab-pane>
+      </a-tabs>
     </div>
+    <a-modal
+      title="添加任务"
+      v-model="isShow"
+    >
+        <div>
+            表单区域
+        </div>
+    </a-modal>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            isShow:false
+        }
+    },
+};
 </script>
 
 <style lang="scss" scoped>
